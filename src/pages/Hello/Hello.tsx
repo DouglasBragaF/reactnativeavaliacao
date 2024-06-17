6145be7e410ca86f526fcb4f86102fcef6076002
Douglas França <douglasbragaf@gmail.com>
2024-06-17 00:16:14 -0300

    component button

--- src/pages/Hello/Hello.tsx
+++ src/pages/Hello/Hello.tsx
@@ -1,7 +1,7 @@
-import { Image, View, Text } from "react-native";
+import { Image, View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import {styles}  from "./styles";
-import { Button } from "../../components/Button/Button";
+import { Button } from "@/src/components/Button/Button";
 
 export const Hello = ({ navigation }: { navigation: any }) => {
   return (
@@ -15,27 +15,35 @@ export const Hello = ({ navigation }: { navigation: any }) => {
         />
         
       </View>
+
       <Text style={styles.title}>Hello</Text>
       <Text style={styles.subtitle}>Welcome To Little Drop, where you manage your daily tasks</Text>
+
       <Button
         title="Login"
         onPress={() => navigation.navigate('Login')}
         style={styles.button}
         textStyle={styles.buttonText}
       />
+
+      
       <Button
-        title="Sign Up"
-        onPress={() => navigation.navigate('SignUp')}
+        title="Sign up"
+        onPress={() => navigation.navigate('Signup')}
         style={styles.outlineButton}
         textStyle={styles.outlineButtonText}
       />
+
       <View style={styles.socialContainer}>
+
         <Text style={styles.socialText}>Sign up using</Text>
+
         <View style={styles.socialIcons}>
           <Image source={{ uri: 'https://img.icons8.com/fluency/48/000000/facebook-new.png' }} style={styles.icon} />
           <Image source={{ uri: 'https://img.icons8.com/fluency/48/000000/google-logo.png' }} style={styles.icon} />
           <Image source={{ uri: 'https://img.icons8.com/fluency/48/000000/linkedin.png' }} style={styles.icon} />
         </View>
+    
       </View>
     </View>
   )};
